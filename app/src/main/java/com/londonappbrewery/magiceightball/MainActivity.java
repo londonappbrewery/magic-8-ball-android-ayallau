6a92f97978850470bf57bf4e4a06708016f9eeb0
Ayal <ayallau@gmail.com>
2019-04-19 04:55:51 +0300

    Project Completed!

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,6 +2,13 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,47 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button askButton;
+        askButton = findViewById(R.id.btn_ask);
+
+        final ImageView magicBall = findViewById(R.id.img_magicBall);
+
+        final int[] answerArray = {
+                R.drawable.ball1,
+                R.drawable.ball2,
+                R.drawable.ball3,
+                R.drawable.ball4,
+                R.drawable.ball5
+        };
+
+
+        askButton.setOnClickListener(new View.OnClickListener() {
+            int number=1;
+            int newNumber;
+            @Override
+            public void onClick(View v) {
+
+                Random randomNumberGenerator = new Random();
+
+                newNumber = randomNumberGenerator.nextInt(5);
+
+                if (newNumber==number)
+                {
+                    if (number==1)
+                        newNumber++;
+                    else if  (number==5)
+                        newNumber--;
+                    else
+                        newNumber++;
+                }
+                number = newNumber;
+
+                magicBall.setImageResource(answerArray[newNumber]);
+            }
+        });
+
+
+
     }
 }
